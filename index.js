5cfc90fbb57285d16476680eb576b9383819b736
AbhaySingh502 <abhaysingh65534@gmail.com>
2023-05-26 16:54:08 +0530

    contact complete

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
-    var typed = new Typed('#element', {
-          strings: ['<i>First</i> sentence.', '&amp; a second sentence.'],
-          typeSpeed: 50,
-        });
-       
+ window.onscroll = () =>{
+  let header = document.querySelector('header');
+  header.classList.toggle('sticky', window.scrolly > 100);
+
+ }      
